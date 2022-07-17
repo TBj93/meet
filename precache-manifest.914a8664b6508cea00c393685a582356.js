fbf5a2a9bba9884e3fe810686ebc68fea961ab2f
Tim Burmester <Tim-Burmester1@web.de>
2022-07-17 19:31:47 +0200

    Updates

---
+++ precache-manifest.914a8664b6508cea00c393685a582356.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d8eb0295367cb810e8b73091b26d04b3",
+    "url": "/meet/index.html"
+  },
+  {
+    "revision": "b8abae36893c4805758c",
+    "url": "/meet/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "47561ff18f7edbbca9ee",
+    "url": "/meet/static/js/2.c20b30d5.chunk.js"
+  },
+  {
+    "revision": "60f6bf9e100e456690e9ab6c9a37bfc2",
+    "url": "/meet/static/js/2.c20b30d5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ad9bdf22b84613c2d19e",
+    "url": "/meet/static/js/3.fdc0b9b7.chunk.js"
+  },
+  {
+    "revision": "b8abae36893c4805758c",
+    "url": "/meet/static/js/main.a7885641.chunk.js"
+  },
+  {
+    "revision": "de291ebc912e1c57ada8",
+    "url": "/meet/static/js/runtime-main.f6c71003.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/meet/static/media/logo.5d5d9eef.svg"
+  }
+]);
