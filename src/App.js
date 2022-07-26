2f7a6269871c1bd52d72370367b6070496494a36
Tim Burmester <Tim-Burmester1@web.de>
2022-07-26 20:16:11 +0200

    4.3: Added event component and first basic test

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React, { Component } from 'react';
 import './App.css';
 import EventList from './EventList'
 import CitySearch from './CitySearch';
+import Event from './Event';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+          <Event />
         <CitySearch/>
       <EventList />
     </div>
--- src/Event.js
+++ src/Event.js
@@ -2,7 +2,14 @@ import React, { Component } from "react";
 
 class Event extends Component {
   render() {
-    return <div></div>;
+    return <div className = "Event">
+
+          <input
+          type="text"
+          className="description"
+        />
+
+    </div>;
   }
 }
 export default Event;
