aa1408710422ceadaefd7b2c29f35005be691e8d
Tim Burmester <Tim-Burmester1@web.de>
2022-07-27 04:33:35 +0200

    4.3: minor import update

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import EventList from './EventList'
 import CitySearch from './CitySearch';
 import Event from './Event';
+import NumberOfEvents from './NumberOfEvents';
 
 class App extends Component {
   render() {
