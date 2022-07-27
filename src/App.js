aa436ccbe03f1579d3625d16361d1af5b80f0ce3
Tim Burmester <Tim-Burmester1@web.de>
2022-07-27 19:38:06 +0200

    4.4: Added first integration tests
    App passes "events" state as a prop to EventList
    
    App passes "locations" state as a prop to CitySearch
    
    get list of events matching the city selected by the user
    
    get list of all events when user selects "See all cities

--- src/App.js
+++ src/App.js
@@ -3,17 +3,32 @@ import './App.css';
 import EventList from './EventList'
 import CitySearch from './CitySearch';
 import Event from './Event';
+import { getEvents } from './api';
 import NumberOfEvents from './NumberOfEvents';
 
 class App extends Component {
+
+
+  updateEvents = (location) => {
+    getEvents().then((events) => {
+      const locationEvents = events.filter((event) => event.location === location);
+      this.setState({
+        events: locationEvents
+      });
+    });
+  }
+  
+  state = {
+    events: [],
+    locations: []
+  }
+
   render() {
     return (
       <div className="App">
-
-          <Event />
-        <CitySearch/>
-        <NumberOfEvents/>
-      <EventList />
+<NumberOfEvents/>  
+<CitySearch locations={this.state.locations} updateEvents={this.updateEvents}/>
+      <EventList events={this.state.events} />
     </div>
     );
   }
