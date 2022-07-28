bf34fd46f71c7e130d64a54082c6854dfac10945
Tim Burmester <Tim-Burmester1@web.de>
2022-07-29 01:24:11 +0200

    Updates

--- precache-manifest.8fb820befa512046d5265dd666cb449b.js
+++ precache-manifest.8fb820befa512046d5265dd666cb449b.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = [
   {
-    "revision": "17b8eedcaf38d0e66faf",
+    "revision": "48efd6e1c6f4264932ce",
     "url": "/meet/static/css/main.b62e5562.chunk.css"
   },
   {
-    "revision": "17b8eedcaf38d0e66faf",
-    "url": "/meet/static/js/main.17b8eedc.chunk.js"
+    "revision": "48efd6e1c6f4264932ce",
+    "url": "/meet/static/js/main.48efd6e1.chunk.js"
   },
   {
     "revision": "877ddeaf8032078f6663",
@@ -20,7 +20,7 @@ self.__precacheManifest = [
     "url": "/meet/static/js/runtime~main.18292d0b.js"
   },
   {
-    "revision": "d9c1a3aa8feb240b833a65dd07f0dbc3",
+    "revision": "a1dcfd55f693f036b5c96b422ad30ed6",
     "url": "/meet/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/meet/precache-manifest.3e88c4aeceb8ebff4597f9fb83bb1f78.js"
+  "/meet/precache-manifest.8fb820befa512046d5265dd666cb449b.js"
 );
 
 workbox.clientsClaim();
