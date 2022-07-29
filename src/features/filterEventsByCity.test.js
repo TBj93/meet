359113426739434d1718b9d41c646edbae263d53
Tim Burmester <Tim-Burmester1@web.de>
2022-07-30 01:03:52 +0200

    4.5: Added auto code from syntax
    added basic acceptance test from terminal for show hide even details

--- src/features/filterEventsByCity.test.js
+++ src/features/filterEventsByCity.test.js
@@ -15,9 +15,10 @@ defineFeature(feature, test => {
   
       });
   
-      when('the user opens the app', () => {
-  
-      });
+      let AppWrapper;
+            when('the user opens the app', () => {
+                AppWrapper = mount(<App />);
+            });
   
       then('the user should see the list of upcoming events.', () => {
         then('the user should see the list of upcoming events from all locations', () => {
@@ -45,25 +46,30 @@ defineFeature(feature, test => {
   });
   
   
-    test('User can select a city from the suggested list', ({ given, and, when, then }) => {
-      given('user was typing “Berlin” in the city textbox', () => {
-  
-      });
-  
-      and('the list of suggested cities is showing', () => {
-  
-      });
-  
-      when('the user selects a city (e.g., “Berlin, Germany”) from the list', () => {
-  
-      });
-  
-      then('their city should be changed to that city (i.e., “Berlin, Germany”)', () => {
-  
-      });
-  
-      and('the user should receive a list of upcoming events in that city', () => {
-  
-      });
+    //Scenario 3
+  test('User can select a city from the suggested list', ({ given, and, when, then }) => {
+    let AppWrapper;
+    given('user was typing “Berlin” in the city textbox', async () => {
+      AppWrapper = await mount(<App />);
+      AppWrapper.find('.city').simulate('change', { target: { value: 'Berlin' } });
+    });
+
+    and('the list of suggested cities is showing', () => {
+      AppWrapper.update();
+      expect(AppWrapper.find('.suggestions li')).toHaveLength(2);
+    });
+
+    when('the user selects a city (e.g., “Berlin, Germany”) from the list', () => {
+      AppWrapper.find('.suggestions li').at(0).simulate('click');
+    });
+
+    then('their city should be changed to that city (i.e., “Berlin, Germany”)', () => {
+      const CitySearchWrapper = AppWrapper.find(CitySearch);
+      expect(CitySearchWrapper.state('query')).toBe('Berlin, Germany');
+    });
+
+    and('the user should receive a list of upcoming events in that city', () => {
+      expect(AppWrapper.find('.event')).toHaveLength(mockData.length);
     });
   });
+    });
---
+++ src/features/showHideAnEventsDetails.test.js
@@ -0,0 +1,58 @@
+import { loadFeature, defineFeature } from 'jest-cucumber';
+import React from 'react';
+import { mount, shallow } from 'enzyme';
+import Event from '../Event';
+import App from '../App';
+import { extractLocations } from "../api";
+import { mockData } from '../mock-data';
+const feature = loadFeature('./src/features/showHideAnEventsDetails.feature');
+
+
+defineFeature(feature, test => {
+
+    test('An event element is collapsed by default.', ({ given, when, then }) => {
+        given('user hasnt searched for any city', () => {
+
+        });
+
+        when('the user opens the app', () => {
+
+        });
+
+        then('all resulting elements should be collapsed by default', () => {
+
+        });
+    });
+
+    test('User can expand an event to see its details.', ({ given, when, then }) => {
+        given('the user has started a search', () => {
+
+        });
+
+        when('the user clicks on the \'show details\' button', () => {
+
+        });
+
+        then('the user should see the details', () => {
+
+        });
+    });
+
+
+test('User can collapse an event to hide its details.', ({ given, when, then }) => {
+        given('the user has opened an events\' details', () => {
+
+        });
+
+        when('the user clicks the \'hide details\' button', () => {
+
+        });
+
+        then('events\' details should collapse', () => {
+
+        });
+    });
+
+
+
+});
