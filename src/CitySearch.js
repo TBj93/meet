be65c5e908e68b696344f4260ba71e8eccd0d030
Tim Burmester <Tim-Burmester1@web.de>
2022-07-31 19:15:55 +0200

    4.7: Added info alert

--- src/CitySearch.js
+++ src/CitySearch.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-
+import { InfoAlert } from './Alert';
 
 class CitySearch extends Component {
 
@@ -15,10 +15,18 @@ class CitySearch extends Component {
         const suggestions = this.props.locations.filter((location) => {
           return location.toUpperCase().indexOf(value.toUpperCase()) > -1;
         });
-        this.setState({
-          query: value,
-          suggestions,
-        });
+        if (suggestions.length === 0) {
+          this.setState({
+            query: value,
+            infoText: 'We can not find the city you are looking for. Please try another city',
+          });
+        } else {
+          return this.setState({
+            query: value,
+            suggestions,
+            infoText:''
+          });
+        }
       };
 
       handleItemClicked = (suggestion) => {
@@ -33,6 +41,7 @@ class CitySearch extends Component {
   render() {
     return (
       <div className="CitySearch">
+          <InfoAlert text={this.state.infoText} />
         <input
           type="text"
           className="city"
